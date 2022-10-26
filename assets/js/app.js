255d5c76641d1181d590293d7610cdec6625bd6b
Pasquale Aragosa <aragosap@gmail.com>
2022-10-26 15:18:13 +0200

    inizial commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const { createApp } = Vue
+
+    createApp({
+        data() {
+        return {
+            message: 'Hello I am Vue, Js framework for web Dev!'
+        }
+        }
+    }).mount('#app')
